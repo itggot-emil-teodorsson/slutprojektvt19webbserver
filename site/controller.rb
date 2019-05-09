21d47378ad747c1eee566207061b0f71bbbadcea
itggot-emil-teodorsson <35598572+itggot-emil-teodorsson@users.noreply.github.com>
2019-05-09 10:57:26 +0200

    Påbörjade dokumentation

--- site/controller.rb
+++ site/controller.rb
@@ -3,6 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require_relative 'model.rb'
+include Model
 
 enable :sessions
 
@@ -12,24 +13,37 @@ before('/') do
     get_posts
 end
 
+# Display Landing Page
+#
 get('/') do
     slim(:index, locals:{users:session[:result], posts:session[:post_text]})
 end
 
+# Calls on a function which upvotes the post and redirects to '/'
+#
+# @see Model#upvote_post_i
 get('/u_vote_i/:postId') do
     upvote_post_i
     redirect back
 end
 
+# Calls on a function which downvotes the post in question
+#
+# @see Model#downvote_post_i
 get('/d_vote_i/:postId') do
     downvote_post_i
     redirect back
 end
 
+# Displays a register form
+#
 get('/registrera') do
     slim(:register)
 end
 
+# Attempts to register login values
+#
+# @see Model#register_values
 post('/register_values') do
     register_values
     if session[:reg_complete] == true
@@ -43,22 +57,33 @@ post('/register_values') do
     end
 end
 
+# Displays a page that tells you that you succeded in registering
+#
 get('/reg_complete') do
     slim(:reg_complete)
 end
 
+# Displays an error message
+#
 get('/reg_failed') do
     slim(:reg_failed)
 end
 
+# Displays an error message
+#
 get('/username_taken') do
     slim(:username_taken)
 end
 
+# Displays a login form
+#
 get('/logga_in') do
     slim(:login)
 end
 
+# Compares the values that you've tried to log in with, with the values that exist in the database and then redirects to either '/' or '/no_access'
+#
+# @see Model#login_values
 post('/login_values') do
     login_values(params["username"], params["password"])
     if session[:valid] == true
@@ -68,10 +93,14 @@ post('/login_values') do
     end
 end
 
+# Displays an error message
+#
 get('/no_access') do
     slim(:no_access)
 end
 
+# Makes it so that you log out and redirects to '/'
+#
 get('/logga_ut') do
     session[:User_id] = nil
     redirect('/')
@@ -81,6 +110,8 @@ before('/skapa_inlagg') do
     login_check
 end
 
+# Displays a form, if you're logged in
+#
 get('/skapa_inlagg') do
     if session[:logged_in] == true
         slim(:skapa_inlagg)
@@ -89,40 +120,62 @@ get('/skapa_inlagg') do
     end
 end
 
+# Attempts to insert the form-values in the database
+#
+# @see Model#upload_post
 post('/uploading_post') do
     upload_post
     redirect('/')
 end
 
+# Displays an individual post
+#
+# @see Model#show_post
 get('/show_post/:postId') do
     show_post
     slim(:show_post, locals:{post_content:session[:PostText], post_user:session[:post_creator], post_upvotes:session[:upvotes], post_userid:session[:UId_P]})
 end
 
+# Calls on a function that upvotes the post when showing a single post and redirects back to '/show_post/:postId'
+#
+# @see Model#upvote_post
 get('/u_vote/:post_id') do
     upvote_post
     redirect back
 end
 
+# Calls on a function that downvotes the post when showing a single post and redirects back to '/show_post/:postId'
+#
+# @see Model#downvote_post
 get('/d_vote/:post_id') do
     downvote_post
     redirect back
 end
 
+# Displays a form for editing your post
+#
 get('/edit_post') do
     slim(:edit_post)
 end
 
+# Updates an existing post and redirects to '/'
+#
+# @see Model#edit_post
 post('/uploading_edit') do
     edit_post
     redirect('/')
 end
 
+# Deletes an existing post and redirects to '/remove_confirmed'
+#
+# @see Model#remove_post
 get('/remove_post/:post_id') do
     remove_post
     redirect('/remove_confirmed')
 end
 
+# Displays a page that tells you that you've succesfully removed a post
+#
 get('/remove_confirmed') do
     slim(:remove_confirmed)
 end
--- site/model.rb
+++ site/model.rb
@@ -1,187 +1,183 @@
-require 'sinatra'
-require 'slim'
-require 'sqlite3'
-require 'bcrypt'
-
-enable :sessions
-
-def login_values(x, y)
-    db=SQLite3::Database.new('db/database.db')
-
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users")
-
-    session[:username] = x
-    session[:password] = y
+module Model
     
-    i = 0
-    while i <= result.length - 1
-        if session[:username] == result[i][1]
-            if BCrypt::Password.new(result[i][2]) == session[:password]
-                session[:valid] = true
-                session[:User_id] = result[i][0]
-                break
+    def login_values(x, y)
+        db=SQLite3::Database.new('db/database.db')
+
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM users")
+
+        session[:username] = x
+        session[:password] = y
+        
+        i = 0
+        while i <= result.length - 1
+            if session[:username] == result[i][1]
+                if BCrypt::Password.new(result[i][2]) == session[:password]
+                    session[:valid] = true
+                    session[:User_id] = result[i][0]
+                    break
+                else
+                    session[:valid] = false
+                end
             else
                 session[:valid] = false
             end
-        else
-            session[:valid] = false
+
+            i += 1
         end
-
-        i += 1
+        
+        return session[:valid]
     end
-    
-    return session[:valid]
-end
 
-def register_values
-    db=SQLite3::Database.new('db/database.db')
-    
-    db.results_as_hash = true
-    usernames = db.execute("SELECT * FROM users")
+    def register_values
+        db=SQLite3::Database.new('db/database.db')
+        
+        db.results_as_hash = true
+        usernames = db.execute("SELECT * FROM users")
 
-    session[:reg_username] = params["reg_username"]
-    session[:reg_password] = params["reg_password"]
-    session[:rereg_password] = params["rereg_password"]
+        session[:reg_username] = params["reg_username"]
+        session[:reg_password] = params["reg_password"]
+        session[:rereg_password] = params["rereg_password"]
 
-    if session[:reg_password] == session[:rereg_password]
+        if session[:reg_password] == session[:rereg_password]
 
-        username_taken = false
-        j = 0
-        while j <= usernames.length - 1
-            if session[:reg_username] == usernames[j][1]
-                username_taken = true
+            username_taken = false
+            j = 0
+            while j <= usernames.length - 1
+                if session[:reg_username] == usernames[j][1]
+                    username_taken = true
+                end
+                j += 1
             end
-            j += 1
-        end
 
-        if username_taken == false
-            session[:hash_password] = BCrypt::Password.create(session[:reg_password])
-            db.execute("INSERT INTO users (Username, Password) VALUES (?,?)", session[:reg_username], session[:hash_password])
+            if username_taken == false
+                session[:hash_password] = BCrypt::Password.create(session[:reg_password])
+                db.execute("INSERT INTO users (Username, Password) VALUES (?,?)", session[:reg_username], session[:hash_password])
+                
+                session[:taken_username] = false
+            else
+                session[:taken_username] = true
+            end
+            session[:reg_complete] = true
+        else
+            session[:reg_complete] = false
+        end
+    end
+
+    def get_username
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
             
-            session[:taken_username] = false
-        else
-            session[:taken_username] = true
-        end
-        session[:reg_complete] = true
-    else
-        session[:reg_complete] = false
+        session[:result] = db.execute("SELECT users.Username FROM users WHERE UserId = ?", session[:User_id])
     end
-end
 
-def get_username
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+    def get_posts
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
         
-    session[:result] = db.execute("SELECT users.Username FROM users WHERE UserId = ?", session[:User_id])
-end
-
-def get_posts
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
-    
-    session[:post_text] = db.execute("SELECT posts.Text, posts.PostId, posts.Upvotes, users.Username FROM posts INNER JOIN users ON users.UserId = posts.UserIdP")
-end
-
-def login_check
-    db=SQLite3::Database.new('db/database.db')
-
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users")
-
-    k = 0
-    
-    while k <= result.length - 1
-        if session[:User_id] == result[k][0]
-            session[:logged_in] = true
-            break
-        else
-            session[:logged_in] = false
-        end
-        
-        k += 1
+        session[:post_text] = db.execute("SELECT posts.Text, posts.PostId, posts.Upvotes, users.Username FROM posts INNER JOIN users ON users.UserId = posts.UserIdP")
     end
-end
 
-def upload_post
-    db=SQLite3::Database.new('db/database.db')
-    
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM posts")
+    def login_check
+        db=SQLite3::Database.new('db/database.db')
 
-    session[:text] = params["p_text"]
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM users")
 
-    db.execute("INSERT INTO posts (Text, UserIdP, Upvotes) VALUES (?,?,?)", session[:text], session[:User_id], 0)
-end
+        k = 0
+        
+        while k <= result.length - 1
+            if session[:User_id] == result[k][0]
+                session[:logged_in] = true
+                break
+            else
+                session[:logged_in] = false
+            end
+            
+            k += 1
+        end
+    end
 
-def show_post
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+    def upload_post
+        db=SQLite3::Database.new('db/database.db')
+        
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM posts")
 
-    session[:post_id] = params["postId"]
+        session[:text] = params["p_text"]
 
-    session[:UId_P] = db.execute("SELECT posts.UserIdP FROM posts WHERE PostId = ?", session[:post_id])
-    session[:PostText] = db.execute("SELECT posts.Text FROM posts WHERE PostId = ?", session[:post_id])
-    session[:upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE PostId = ?", session[:post_id])
-    session[:post_creator] = db.execute("SELECT users.Username FROM users WHERE UserId = ?", session[:UId_P][0]["UserIdP"])
-end
+        db.execute("INSERT INTO posts (Text, UserIdP, Upvotes) VALUES (?,?,?)", session[:text], session[:User_id], 0)
+    end
 
-def upvote_post
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+    def show_post
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-    session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", session[:post_id])
+        session[:post_id] = params["postId"]
 
-    session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i + 1
+        session[:UId_P] = db.execute("SELECT posts.UserIdP FROM posts WHERE PostId = ?", session[:post_id])
+        session[:PostText] = db.execute("SELECT posts.Text FROM posts WHERE PostId = ?", session[:post_id])
+        session[:upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE PostId = ?", session[:post_id])
+        session[:post_creator] = db.execute("SELECT users.Username FROM users WHERE UserId = ?", session[:UId_P][0]["UserIdP"])
+    end
 
-    db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], session[:post_id])
-end
+    def upvote_post
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-def downvote_post
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+        session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", session[:post_id])
 
-    session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", session[:post_id])
+        session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i + 1
 
-    session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i - 1
+        db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], session[:post_id])
+    end
 
-    db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], session[:post_id])
-end
+    def downvote_post
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-def upvote_post_i
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+        session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", session[:post_id])
 
-    session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", params["postId"])
+        session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i - 1
 
-    session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i + 1
+        db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], session[:post_id])
+    end
 
-    db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], params["postId"])
-end
+    def upvote_post_i
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-def downvote_post_i
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+        session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", params["postId"])
 
-    session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", params["postId"])
+        session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i + 1
 
-    session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i - 1
+        db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], params["postId"])
+    end
 
-    db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], params["postId"])
-end
+    def downvote_post_i
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-def edit_post
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+        session[:current_upvotes] = db.execute("SELECT posts.Upvotes FROM posts WHERE postId = ?", params["postId"])
 
-    session[:new_text] = params["e_text"]
+        session[:new_upvotes] = session[:current_upvotes][0]["Upvotes"].to_i - 1
 
-    db.execute("UPDATE posts SET Text = ? WHERE postId = ?", session[:new_text], session[:post_id])
-end
+        db.execute("UPDATE posts SET Upvotes = ? WHERE postId = ?", session[:new_upvotes], params["postId"])
+    end
 
-def remove_post
-    db = SQLite3::Database.new("db/database.db")
-    db.results_as_hash = true
+    def edit_post
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
 
-    db.execute("DELETE FROM posts WHERE postId = ?", session[:post_id])
+        session[:new_text] = params["e_text"]
+
+        db.execute("UPDATE posts SET Text = ? WHERE postId = ?", session[:new_text], session[:post_id])
+    end
+
+    def remove_post
+        db = SQLite3::Database.new("db/database.db")
+        db.results_as_hash = true
+
+        db.execute("DELETE FROM posts WHERE postId = ?", session[:post_id])
+    end
 end
